0a82b0da3d0b2ab6ff181777daa3a597ce91d66b
archnode <markus@aichinger.dev>
2025-12-07 19:07:41 +0100

    Update for HRM settings.

--- src/main.rs
+++ src/main.rs
@@ -13,21 +13,25 @@ use embassy_rp::flash::{Async, Flash};
 use embassy_rp::gpio::{Flex, Input, Level};
 use embassy_rp::peripherals::USB;
 use embassy_rp::usb::{Driver, InterruptHandler};
-use log::info;
+use embassy_time::Duration;
 use keymap::{COL, ROW};
-use rmk::types::action::{EncoderAction, MorseProfile};
-use rmk::matrix::bidirectional_matrix::{BidirectionalMatrix, ScanLocation};
+use log::info;
 use rmk::channel::EVENT_CHANNEL;
-use rmk::config::{BehaviorConfig, DeviceConfig, MorsesConfig, PositionalConfig, RmkConfig, StorageConfig, VialConfig};
+use rmk::config::{
+    BehaviorConfig, DeviceConfig, MorsesConfig, PositionalConfig, RmkConfig, StorageConfig,
+    VialConfig,
+};
 use rmk::debounce::default_debouncer::DefaultDebouncer;
 use rmk::futures::future::join3;
-use rmk::input_device::rotary_encoder::{RotaryEncoder};
+use rmk::input_device::rotary_encoder::RotaryEncoder;
 use rmk::input_device::Runnable;
 use rmk::keyboard::Keyboard;
+use rmk::matrix::bidirectional_matrix::{BidirectionalMatrix, ScanLocation};
+use rmk::types::action::{EncoderAction, MorseMode, MorseProfile};
 use rmk::{initialize_encoder_keymap_and_storage, run_devices, run_rmk};
 use vial::{VIAL_KEYBOARD_DEF, VIAL_KEYBOARD_ID};
 
-use {panic_probe as _};
+use panic_probe as _;
 
 bind_interrupts!(struct Irqs {
     USBCTRL_IRQ => InterruptHandler<USB>;
@@ -37,7 +41,7 @@ const FLASH_SIZE: usize = 2 * 1024 * 1024;
 const PIN_NUM: usize = 10;
 
 #[embassy_executor::main]
-async fn main(_spawner: Spawner) {    
+async fn main(_spawner: Spawner) {
     info!("RMK start!");
     // Initialize peripherals
     let p = embassy_rp::init(Default::default());
@@ -46,33 +50,80 @@ async fn main(_spawner: Spawner) {
     let driver = Driver::new(p.USB, Irqs);
 
     // Pin config
-    let flex_pins =
-        config_matrix_pins_rp!(peripherals: p, pins: [
-            // Rows Index 0-3
-            PIN_26, PIN_15, PIN_14, PIN_13,
-            // Cols Index 4-9
-            PIN_29, PIN_28, PIN_27, PIN_12, PIN_11, PIN_10
-        ]);
-    
+    let flex_pins = config_matrix_pins_rp!(peripherals: p, pins: [
+        // Rows Index 0-3
+        PIN_26, PIN_15, PIN_14, PIN_13,
+        // Cols Index 4-9
+        PIN_29, PIN_28, PIN_27, PIN_12, PIN_11, PIN_10
+    ]);
+
     let scan_map = [
         // Row 1: Left
-        [ScanLocation::Pins(0,4), ScanLocation::Pins(4,0), ScanLocation::Pins(0,5), ScanLocation::Pins(5,0), ScanLocation::Pins(0,6), ScanLocation::Pins(6,0),
-        // Row 1: Right
-        ScanLocation::Pins(7,0), ScanLocation::Pins(0,7), ScanLocation::Pins(8,0), ScanLocation::Pins(0,8), ScanLocation::Pins(9,0), ScanLocation::Pins(0,9)],
+        [
+            ScanLocation::Pins(0, 4),
+            ScanLocation::Pins(4, 0),
+            ScanLocation::Pins(0, 5),
+            ScanLocation::Pins(5, 0),
+            ScanLocation::Pins(0, 6),
+            ScanLocation::Pins(6, 0),
+            // Row 1: Right
+            ScanLocation::Pins(7, 0),
+            ScanLocation::Pins(0, 7),
+            ScanLocation::Pins(8, 0),
+            ScanLocation::Pins(0, 8),
+            ScanLocation::Pins(9, 0),
+            ScanLocation::Pins(0, 9),
+        ],
         // Row 2: Left
-        [ScanLocation::Pins(1,4), ScanLocation::Pins(4,1), ScanLocation::Pins(1,5), ScanLocation::Pins(5,1), ScanLocation::Pins(1,6), ScanLocation::Pins(6,1),
-        // Row 2: Right
-        ScanLocation::Pins(7,1), ScanLocation::Pins(1,7), ScanLocation::Pins(8,1), ScanLocation::Pins(1,8), ScanLocation::Pins(9,1), ScanLocation::Pins(1,9)],
+        [
+            ScanLocation::Pins(1, 4),
+            ScanLocation::Pins(4, 1),
+            ScanLocation::Pins(1, 5),
+            ScanLocation::Pins(5, 1),
+            ScanLocation::Pins(1, 6),
+            ScanLocation::Pins(6, 1),
+            // Row 2: Right
+            ScanLocation::Pins(7, 1),
+            ScanLocation::Pins(1, 7),
+            ScanLocation::Pins(8, 1),
+            ScanLocation::Pins(1, 8),
+            ScanLocation::Pins(9, 1),
+            ScanLocation::Pins(1, 9),
+        ],
         // Row 3: Left
-        [ScanLocation::Pins(2,4), ScanLocation::Pins(4,2), ScanLocation::Pins(2,5), ScanLocation::Pins(5,2), ScanLocation::Pins(2,6), ScanLocation::Pins(6,2),
-        // Row 3: Right
-        ScanLocation::Pins(7,2), ScanLocation::Pins(2,7), ScanLocation::Pins(8,2), ScanLocation::Pins(2,8), ScanLocation::Pins(9,2), ScanLocation::Pins(2,9)],
+        [
+            ScanLocation::Pins(2, 4),
+            ScanLocation::Pins(4, 2),
+            ScanLocation::Pins(2, 5),
+            ScanLocation::Pins(5, 2),
+            ScanLocation::Pins(2, 6),
+            ScanLocation::Pins(6, 2),
+            // Row 3: Right
+            ScanLocation::Pins(7, 2),
+            ScanLocation::Pins(2, 7),
+            ScanLocation::Pins(8, 2),
+            ScanLocation::Pins(2, 8),
+            ScanLocation::Pins(9, 2),
+            ScanLocation::Pins(2, 9),
+        ],
         // Row 4: Left
-        [ScanLocation::Pins(3,4), ScanLocation::Pins(4,3), ScanLocation::Pins(3,5), ScanLocation::Pins(5,3), ScanLocation::Pins(3,6), ScanLocation::Ignore,
-        // Row 4: Right
-        ScanLocation::Ignore, ScanLocation::Ignore, ScanLocation::Pins(8,3), ScanLocation::Pins(3,8), ScanLocation::Pins(9,3), ScanLocation::Pins(3,9)],
+        [
+            ScanLocation::Pins(3, 4),
+            ScanLocation::Pins(4, 3),
+            ScanLocation::Pins(3, 5),
+            ScanLocation::Pins(5, 3),
+            ScanLocation::Pins(3, 6),
+            ScanLocation::Ignore,
+            // Row 4: Right
+            ScanLocation::Ignore,
+            ScanLocation::Ignore,
+            ScanLocation::Pins(8, 3),
+            ScanLocation::Pins(3, 8),
+            ScanLocation::Pins(9, 3),
+            ScanLocation::Pins(3, 9),
+        ],
     ];
-    
+
     // Use internal flash to emulate eeprom
     // Both blocking and async flash are support, use different API
     // let flash = Flash::<_, Blocking, FLASH_SIZE>::new_blocking(p.FLASH);
@@ -99,22 +150,35 @@ async fn main(_spawner: Spawner) {
     let storage_config = StorageConfig::default();
     let mut per_key_config = PositionalConfig::default();
     let mut behavior_config = BehaviorConfig {
-      morse: MorsesConfig {
-          enable_flow_tap: true,
-          default_profile: MorseProfile::default().with_hold_timeout_ms(Some(185)),
-          ..Default::default()
-      },
-      ..Default::default()  
+        morse: MorsesConfig {
+            enable_flow_tap: true,
+            prior_idle_time: Duration::from_millis(200),
+            default_profile: MorseProfile::default()
+                .with_hold_timeout_ms(Some(185))
+                .with_unilateral_tap(Some(true))
+                .with_gap_timeout_ms(Some(250))
+                .with_mode(Some(MorseMode::PermissiveHold)),
+            ..Default::default()
+        },
+        ..Default::default()
     };
     let mut encoder_map: [[EncoderAction; _]; _] = keymap::get_default_encoder_map();
-    let (keymap, mut storage) =
-        initialize_encoder_keymap_and_storage(&mut default_keymap, &mut encoder_map, flash, &storage_config, &mut behavior_config, &mut per_key_config).await;
+    let (keymap, mut storage) = initialize_encoder_keymap_and_storage(
+        &mut default_keymap,
+        &mut encoder_map,
+        flash,
+        &storage_config,
+        &mut behavior_config,
+        &mut per_key_config,
+    )
+    .await;
 
     // Initialize the matrix + keyboard
     let debouncer = DefaultDebouncer::<ROW, COL>::new();
-    let mut matrix = BidirectionalMatrix::<_, _, PIN_NUM, ROW, COL>::new(flex_pins, debouncer, scan_map);
+    let mut matrix =
+        BidirectionalMatrix::<_, _, PIN_NUM, ROW, COL>::new(flex_pins, debouncer, scan_map);
     let mut keyboard = Keyboard::new(&keymap);
-    
+
     // Initialize Rotary Encoder
     let pin_a = Input::new(p.PIN_1, embassy_rp::gpio::Pull::Up);
     let pin_b = Input::new(p.PIN_2, embassy_rp::gpio::Pull::Up);
